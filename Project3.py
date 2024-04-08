c91f1c0a791f80608b7b7028b339077af7bdfffe
Alex Lusma <142031846+alexlusma1@users.noreply.github.com>
2024-04-08 10:18:07 -0400

    Project3.py

--- Project3.py
+++ Project3.py
@@ -1,3 +1,8 @@
+Create a function that accepts 3 parameters and checks for equality between any two of them.
+Your function should return True if 2 or more of the parameters are equal,
+and false is none of them are equal to any of the others.
+
+
 def check_equality(param1, param2, param3):
     if param1 == param2 or param1 == param3 or param2 == param3:
         return True
