8ef8c72edbb465a09331faa840c7e3051c7b4771
Alex Lusma <142031846+alexlusma1@users.noreply.github.com>
2024-04-08 10:25:10 -0400

    Add files via upload

---
+++ Project6.py
@@ -0,0 +1,18 @@
+song_info = {
+    "Artist": "Kanye West",
+    "Genre": "Rap",
+    "DurationInSeconds": "240",
+    "Location": "United States",
+    "YearReleased": "2023",
+    "Beat": "808",
+    "Clothes": "Black",
+    "Hook": "Go, Go, Go",
+    "Scenery": "Smoke",
+    "Fans": "Inter Milan Ultras",
+    "Audience": "Worldwide",
+    "Language": "Mature Audience",
+    "InvitedGuests": "Rich The Kid, Playboi Carti, Ty Dolla Sign"
+}
+
+for key, value in song_info.items():
+    print(f"{key}: {value}")
