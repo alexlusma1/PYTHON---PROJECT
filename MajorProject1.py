7fe0ed476c417bf5d15feb9cb565120150260076
Alex Lusma <142031846+alexlusma1@users.noreply.github.com>
2024-04-08 10:33:08 -0400

    Update MajorProject1.py

--- MajorProject1.py
+++ MajorProject1.py
@@ -1,6 +1,4 @@
-
-
-your task is create a Connect 4 game in Python. Once you've got the rules down, your assignment should be fairly straightforward. 
+Your task is create a Connect 4 game in Python. Once you've got the rules down, your assignment should be fairly straightforward. 
 You'll want to draw the board, and allow two players to take turns placing their pieces on the board 
 (but as you learned above, they can only do so by choosing a column, not a row). The first player to get 4 across or diagonal should win!
 Normally the pieces would be red and black.
