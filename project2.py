ebb0d15ef61c0717077024d2065d9896f86c8dee
Alex Lusma <142031846+alexlusma1@users.noreply.github.com>
2024-04-08 10:12:39 -0400

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,12 @@
+def Artist ():
+    return "Kanye West"
+
+print(Artist())
+
+def Genre ():
+    return "Rap"
+print(Genre())
+
+def DurationInSeconds ():
+    return "240"
+print(DurationInSeconds())
