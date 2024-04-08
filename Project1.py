20189bf4387cdbf9367b5bb8f576831cdce3d64a
Alex Lusma <142031846+alexlusma1@users.noreply.github.com>
2024-04-08 10:09:46 -0400

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,31 @@
+
+
+
+#Kanye West single " Carnival" featuring Rich The Kid, Playboi Carti, Ty Dolla Sign
+Artist ="Kanye West"
+Genre = "Rap"
+DurationInSeconds = "240"
+Location = "United States"
+YearReleased = "2023"
+Beat = "808"
+Clothes = "Black"
+Hook = "Go, Go, Go"
+Scenery = "Smoke"
+Fans = "Inter Milan Ultras"
+Audience = "Worldwide"
+Language = "Mature Audience"
+InvitedGuests = "Rich The Kid, Playboi Carti, Ty Dolla Sign"
+
+print(Artist)
+print(Genre)
+print(DurationInSeconds)
+print(Location)
+print(YearReleased)
+print(Beat)
+print(Clothes)
+print(Hook)
+print(Scenery)
+print(Fans)
+print(Audience)
+print(Language)
+print(InvitedGuests)
